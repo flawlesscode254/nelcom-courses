87c3870c29d742e47ee99dbbcceafb6df02ff432
flawlesscode254 <denzme414@gmail.com>
2021-11-16 15:26:53 +0300

    fixed some bugs

--- src/layouts/dashboard/SidebarConfig.js
+++ src/layouts/dashboard/SidebarConfig.js
@@ -1,6 +1,6 @@
 import { Icon } from '@iconify/react';
 import shoppingBagFill from '@iconify/icons-eva/shopping-bag-fill';
-import fileTextFill from '@iconify/icons-eva/file-text-fill';
+// import fileTextFill from '@iconify/icons-eva/file-text-fill';
 
 const getIcon = (name) => <Icon icon={name} width={22} height={22} />;
 
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
 
 import Products from './pages/Products';
-import Blog from './pages/Blog';
+// import Blog from './pages/Blog';
 import NotFound from './pages/Page404';
 
 // ----------------------------------------------------------------------
