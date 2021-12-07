43cc28df7372e2a9c8e48be0194c0f6c1bf05d39
flawlesscode254 <denzme414@gmail.com>
2021-12-07 15:13:14 +0300

    added a new firebase config and added the firebase file to gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/auth';
 import 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBWeUb95S8Dd8IvaCAHvsYTx-BQLe3hI-8",
-  authDomain: "t-courses.firebaseapp.com",
-  projectId: "t-courses",
-  storageBucket: "t-courses.appspot.com",
-  messagingSenderId: "67895789170",
-  appId: "1:67895789170:web:5f0faa5bdc229e7d3ae4eb"
+  apiKey: "AIzaSyCot5s964tj4RBafWmqzzHRwjqYWBuQ7xQ",
+  authDomain: "nelcom-88e0c.firebaseapp.com",
+  projectId: "nelcom-88e0c",
+  storageBucket: "nelcom-88e0c.appspot.com",
+  messagingSenderId: "87810789028",
+  appId: "1:87810789028:web:ba57a15409591f3582a031"
 };
 
 firebase.initializeApp(firebaseConfig);
